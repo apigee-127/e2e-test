77bee1b741b0bd62235df8af0ff47eeef82c31c8
Mohsen Azimi <msn.azi@gmail.com>
2014-12-29 14:37:51 -0800

    Initial commit
    Add basic test that ensures `npm install -g apigee-127` installs a127
    binary. This commit is for trying TravisCI configurations

---
+++ test/installation.js
@@ -0,0 +1,24 @@
+var exec = require('child_process').exec;
+var expect = require('chai').expect;
+
+describe('Installation', function() {
+  it('should install a127 executable globally', function(done) {
+    exec('a127', function(error, stdout, stderr){
+      expect(error).to.be.falsy;
+      done();
+    });
+  });
+
+  it('should print help when executing `a127`', function(done) {
+    exec('a127', function(error, stdout, stderr) {
+      expect(stdout).to.contain('Usage: a127 [options] [command]');
+      expect(stdout).to.contain('account <action>');
+      expect(stdout).to.contain('project <action>');
+      expect(stdout).to.contain('usergrid <action>');
+      expect(stdout).to.contain('config');
+      expect(stdout).to.contain('wiki');
+      expect(stdout).to.contain('help [cmd]');
+      done();
+    });
+  });
+});
