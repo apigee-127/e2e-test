3fc8e179fe0913ccc8aef7c0c795e8ae893a8ce5
Mohsen Azimi <msn.azi@gmail.com>
2014-12-30 17:40:35 -0800

    Add tests for start server

--- test/project.js
+++ test/project.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
 var path = require('path');
-var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 var expect = require('chai').expect;
 var mkdirp = require('mkdirp');
+var request = require('request');
 
 describe('project', function() {
   var rand = Math.random().toString(36).substr(2);
@@ -13,6 +14,7 @@ describe('project', function() {
 
   describe('create', function() {
     it('makes a folder for a127 project', function (done) {
+
       mkdirp(cwd, function (error){
         expect(error).to.be.falsy;
         expect(fs.existsSync(cwd)).to.be.true;
@@ -20,12 +22,12 @@ describe('project', function() {
       });
     });
 
-    it('makes hello-world app by executing `a127 project create hello-world`',
-      function (done) {
+    it('makes hello-world app by executing `a127 project create hello-world`', function (done) {
         this.timeout(60 * 1000);
-        var create = exec('a127 project create hello-world', {cwd: cwd},
-          function (error, stdout, stderr) {
+
+        var create = spawn('a127',['project', 'create', 'hello-world'], {cwd: cwd}, function (error, stdout, stderr) {
             expect(error).to.be.falsy;
+            expect(stderr).to.be.falsy;
             expect(stdout).to.contain('Project hello-world created');
         });
 
@@ -38,12 +40,36 @@ describe('project', function() {
 
     it('cd into hello-world directory', function(){
       cwd = path.join(cwd, 'hello-world');
+
       expect(fs.existsSync(cwd)).to.be.true;
       expect(fs.lstatSync(cwd).isDirectory()).to.be.true;
     });
   });
 
-  xdescribe('start', function() {
+  describe('start', function() {
+    var serverProccess;
 
+    it('uses `a127 project start` to start the project', function (done) {
+      var output = '';
+
+      serverProccess = spawn('a127', ['project', 'start'], {cwd: cwd});
+
+      // wait 1 second for server to start then go to next test
+      setTimeout(done, 1000);
+    });
+
+    it('responds to simple API request', function (done) {
+      request('http://127.0.0.1:10010/hello?name=Scott', function (error, response, body) {
+
+          expect(error).to.be.falsy;
+          expect(body).to.contain('Hello, Scott');
+          done();
+        });
+    });
+
+    it('kills the server process', function () {
+      serverProccess.kill();
+      expect(serverProccess.connected).to.be.false;
+    });
   });
 });
