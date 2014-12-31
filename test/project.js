fe990afa5e3a2edb65e95629eba7989b538f2456
Mohsen Azimi <msn.azi@gmail.com>
2014-12-31 13:23:02 -0800

    All tests are passing.
    There is an issue with process.kill

--- test/project.js
+++ test/project.js
@@ -10,6 +10,9 @@ var request = require('request');
 describe('project', function() {
   var rand = Math.random().toString(36).substr(2);
   var cwd = path.join(__dirname, 'tmp', rand);
+  var editServerUrl = null;
+  var serverProccess;
+  var editProccess;
 
   console.log('testing in', cwd);
   this.timeout(5000);
@@ -17,7 +20,7 @@ describe('project', function() {
 
   function startEdit(){
     it('starts the edit server with `a127 project edit` command', function (done) {
-      serverProccess = exec('a127 project edit', {cwd: cwd}, function (error, stdout, stderr) {
+      editProccess = exec('a127 project edit', {cwd: cwd}, function (error, stdout, stderr) {
 
         // after killing edit there should be no error
         expect(error).to.be.falsy;
@@ -25,13 +28,19 @@ describe('project', function() {
       });
 
       var output = '';
-      serverProccess.stdout.on('data', function (data) { output += data; });
+      editProccess.stdout.on('data', function (data) { output += data; });
 
       // wait 1 second for server to start then go to next test
       setTimeout(function () {
         expect(output).to.contain('Starting Swagger editor.');
+        output.split('\n').forEach(function (line) {
+          if (line.indexOf('Opening browser to:') > -1 ) {
+            editServerUrl = line.substr(line.indexOf('to: ') + 4);
+            editServerUrl = editServerUrl.substr(0, editServerUrl.indexOf('#')) + 'editor/spec';
+          }
+        });
         done();
-      }, 1000);
+      }, 3000);
     });
   }
 
@@ -52,6 +61,29 @@ describe('project', function() {
         expect(output).to.contain('project started');
         done();
       }, 1000);
+    });
+  }
+
+  function restartServer() {
+    it('restarts the server', function (done){
+      serverProccess.stdin.write('re');
+      setTimeout(done, 1000);
+    });
+  };
+
+  function killServer() {
+    it('kills the server process', function (done) {
+      serverProccess.kill()
+      expect(serverProccess.connected).to.be.false;
+      setTimeout(done, 1000);
+    });
+  }
+
+  function killEditServer() {
+     it('kills the edit server process', function (done) {
+      editProccess.kill();
+      expect(editProccess.connected).to.be.false;
+      setTimeout(done, 1000);
     });
   }
 
@@ -90,8 +122,6 @@ describe('project', function() {
   });
 
   describe('start', function() {
-    var serverProccess;
-
     startServer();
 
     it('responds to a simple API request', function (done) {
@@ -103,20 +133,40 @@ describe('project', function() {
         });
     });
 
-    it('kills the server process', function () {
-      process.kill(serverProccess);
-      expect(serverProccess.connected).to.be.false;
-    });
+   killServer();
+
   });
 
-  xdescribe('edit', function () {
+  describe('edit', function () {
     startServer();
     startEdit();
 
-    it('updates the /hello path to /my-path', function () {
-      var updatedPathYaml = fs.readFileSync(path.join(__dirname, '..', '1_updated_path.yaml')).toString();
-      // request.put()
-      expect(true).to.be.false;
+    it('updates the /hello path to /my-path', function (done) {
+      var filePath = path.join(__dirname, '..', 'yaml/1_updated_path.yaml');
+      var yamlFile = fs.readFileSync(filePath).toString();
+
+      request({
+        method: 'PUT',
+        url: editServerUrl,
+        body: yamlFile
+      }, function (error, response, body) {
+        expect(error).to.be.falsy;
+        done();
+      });
     });
+
+    restartServer();
+
+    it('makes call to /my-path', function (done) {
+      request('http://127.0.0.1:10010/my-path?name=Scott', function (error, response, body) {
+
+        expect(error).to.be.falsy;
+        expect(body).to.contain('Hello, Scott');
+        done();
+      });
+    });
+
+    killServer();
+    killEditServer();
   });
 });
