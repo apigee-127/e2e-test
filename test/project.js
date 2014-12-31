a8dd82b1246e8d93e8a42476c62a0c28583c85ac
Mohsen Azimi <msn.azi@gmail.com>
2014-12-31 12:14:27 -0800

    Move startServer and startEdit to a function

--- test/project.js
+++ test/project.js
@@ -1,9 +1,11 @@
 var fs = require('fs');
 var path = require('path');
-var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 var expect = require('chai').expect;
 var mkdirp = require('mkdirp');
 var request = require('request');
+
+
 
 describe('project', function() {
   var rand = Math.random().toString(36).substr(2);
@@ -11,6 +13,47 @@ describe('project', function() {
 
   console.log('testing in', cwd);
   this.timeout(5000);
+
+
+  function startEdit(){
+    it('starts the edit server with `a127 project edit` command', function (done) {
+      serverProccess = exec('a127 project edit', {cwd: cwd}, function (error, stdout, stderr) {
+
+        // after killing edit there should be no error
+        expect(error).to.be.falsy;
+        expect(stderr).to.be.falsy;
+      });
+
+      var output = '';
+      serverProccess.stdout.on('data', function (data) { output += data; });
+
+      // wait 1 second for server to start then go to next test
+      setTimeout(function () {
+        expect(output).to.contain('Starting Swagger editor.');
+        done();
+      }, 1000);
+    });
+  }
+
+  function startServer() {
+    it('starts the server with `a127 project start` command', function (done) {
+      serverProccess = exec('a127 project start', {cwd: cwd}, function (error, stdout, stderr) {
+
+        // after killing server there should be no errors
+        expect(error).to.be.falsy;
+        expect(stderr).to.be.falsy;
+      });
+
+      var output = '';
+      serverProccess.stdout.on('data', function (data) { output += data; });
+
+      // wait 1 second for server to start then go to next test
+      setTimeout(function () {
+        expect(output).to.contain('project started');
+        done();
+      }, 1000);
+    });
+  }
 
   describe('create', function() {
     it('makes a folder for a127 project', function (done) {
@@ -25,13 +68,13 @@ describe('project', function() {
     it('makes hello-world app by executing `a127 project create hello-world`', function (done) {
         this.timeout(60 * 1000);
 
-        var create = spawn('a127',['project', 'create', 'hello-world'], {cwd: cwd}, function (error, stdout, stderr) {
+        var create = exec('a127 project create hello-world', {cwd: cwd}, function (error, stdout, stderr) {
             expect(error).to.be.falsy;
             expect(stderr).to.be.falsy;
             expect(stdout).to.contain('Project hello-world created');
         });
 
-        create.on('exit', function (code, signal) {
+        create.on('close', function (code, signal) {
           expect(code).to.equal(0);
           expect(signal).to.be.null;
           done();
@@ -49,16 +92,9 @@ describe('project', function() {
   describe('start', function() {
     var serverProccess;
 
-    it('uses `a127 project start` to start the project', function (done) {
-      var output = '';
+    startServer();
 
-      serverProccess = spawn('a127', ['project', 'start'], {cwd: cwd});
-
-      // wait 1 second for server to start then go to next test
-      setTimeout(done, 1000);
-    });
-
-    it('responds to simple API request', function (done) {
+    it('responds to a simple API request', function (done) {
       request('http://127.0.0.1:10010/hello?name=Scott', function (error, response, body) {
 
           expect(error).to.be.falsy;
@@ -70,6 +106,17 @@ describe('project', function() {
     it('kills the server process', function () {
       process.kill(serverProccess);
       expect(serverProccess.connected).to.be.false;
+    });
+  });
+
+  xdescribe('edit', function () {
+    startServer();
+    startEdit();
+
+    it('updates the /hello path to /my-path', function () {
+      var updatedPathYaml = fs.readFileSync(path.join(__dirname, '..', '1_updated_path.yaml')).toString();
+      // request.put()
+      expect(true).to.be.false;
     });
   });
 });
