484df2ad5866cb9fe4f51cc8494d86d9ec2c23ff
Mohsen Azimi <msn.azi@gmail.com>
2014-12-30 17:45:46 -0800

    un-comment installation

--- test/installation.js
+++ test/installation.js
@@ -1,26 +1,26 @@
-// var exec = require('child_process').exec;
-// var expect = require('chai').expect;
+var exec = require('child_process').exec;
+var expect = require('chai').expect;
 
-// describe('Installation', function() {
-//   this.timeout(50000);
+describe('Installation', function() {
+  this.timeout(50000);
 
-//   it('should install a127 executable globally', function(done) {
-//     exec('npm install -g apigee-127', function(error, stdout, stderr){
-//       expect(error).to.be.falsy;
-//       done();
-//     });
-//   });
+  it('should install a127 executable globally', function(done) {
+    exec('npm install -g apigee-127', function(error, stdout, stderr){
+      expect(error).to.be.falsy;
+      done();
+    });
+  });
 
-//   it('should print help when executing `a127`', function(done) {
-//     exec('a127', function(error, stdout, stderr) {
-//       expect(stdout).to.contain('Usage: a127 [options] [command]');
-//       expect(stdout).to.contain('account <action>');
-//       expect(stdout).to.contain('project <action>');
-//       expect(stdout).to.contain('usergrid <action>');
-//       expect(stdout).to.contain('config');
-//       expect(stdout).to.contain('wiki');
-//       expect(stdout).to.contain('help [cmd]');
-//       done();
-//     });
-//   });
-// });
+  it('should print help when executing `a127`', function(done) {
+    exec('a127', function(error, stdout, stderr) {
+      expect(stdout).to.contain('Usage: a127 [options] [command]');
+      expect(stdout).to.contain('account <action>');
+      expect(stdout).to.contain('project <action>');
+      expect(stdout).to.contain('usergrid <action>');
+      expect(stdout).to.contain('config');
+      expect(stdout).to.contain('wiki');
+      expect(stdout).to.contain('help [cmd]');
+      done();
+    });
+  });
+});
