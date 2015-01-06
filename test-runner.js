38517527c71b9cdc3b672f755a49e43f828495f5
Mohsen Azimi <msn.azi@gmail.com>
2015-01-06 13:49:12 -0800

    Fix JS Code Style issues

--- test-runner.js
+++ test-runner.js
@@ -1,23 +1,23 @@
 'use strict';
 
-var Mocha = require('mocha'),
-    mocha = new Mocha(),
-    fs = require('fs'),
-    path = require('path'),
-    TEST_DIR = 'test';
+var Mocha = require('mocha');
+var mocha = new Mocha();
+var fs = require('fs');
+var path = require('path');
+var TEST_DIR = 'test';
 
-fs.readdirSync(TEST_DIR).filter(function(file){
-    // Only keep the .js files
-    return file.substr(-3) === '.js';
+fs.readdirSync(TEST_DIR).filter(function(file) {
+  // Only keep the .js files
+  return file.substr(-3) === '.js';
 
-}).forEach(function(file){
-    // Use the method "addFile" to add the file to mocha
-    mocha.addFile(path.join(TEST_DIR, file));
+}).forEach(function(file) {
+  // Use the method "addFile" to add the file to mocha
+  mocha.addFile(path.join(TEST_DIR, file));
 });
 
 // Now, you can run the tests.
-mocha.run(function(failures){
-  process.on('exit', function () {
+mocha.run(function(failures) {
+  process.on('exit', function() {
     process.exit(failures);
   });
 });
