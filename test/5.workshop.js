033de104af4dbc48884e27973aec94c1ffef36fb
Mohsen Azimi <msn.azi@gmail.com>
2015-01-09 15:17:10 -0800

    Add workshop tests

---
+++ test/5.workshop.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+var path = require('path');
+var chai = require('chai');
+var request = require('request');
+var running = require('is-running');
+var expect = chai.expect;
+var TIMEOUT = process.env.TIMEOUT || 3000;
+var deleteAccount = require('./3.account').delete;
+var cwd = path.join(__dirname, '..', 'apigee-api-workshop');
+
+describe('workshop', function() {
+  var serverProccess = null;
+
+  this.timeout(2 * TIMEOUT);
+
+  it('start the project server', function(done) {
+    serverProccess = spawn('a127', ['project', 'start'], {cwd: cwd, detached: true});
+
+    var output = '';
+    serverProccess.stdout.on('data', function(data) { output += data; });
+
+    setTimeout(function() {
+      expect(output).to.contain('project started');
+      done();
+    }, TIMEOUT);
+  });
+
+  it('makes an HTTP GET request to /hello path', function(done) {
+    request('http://localhost:8888/hello?name=Peter', function(error, response, body) {
+      expect(error).to.be.falsy;
+      expect(body).to.contain('Hello, Peter');
+      done();
+    });
+  });
+
+  xit('makes an HTTP GET request to /restaurants path');
+
+  it('deploys the API to Apigee by executing `a127 project deploy`', function(done) {
+    exec('127 project deploy', {cwd: cwd}, function(error, stdout, stderr) {
+      expect(error).to.be.falsy;
+      expect(stderr).to.be.falsy;
+      done();
+    });
+  });
+
+  it('makes a call to deployed API to make sure deployed API is working', function(done) {
+    var url = 'http://mazimi_a127-test.apigee.net/apigee-api-workshop/my-path?name=Bart&last=Simpson';
+    request(url, function(error, resp, body) {
+      expect(error).to.be.falsy;
+      expect(body).to.contain('Hello, Bart Simpson');
+      done();
+    });
+  });
+
+  it('undeploys the API from Apigee by executing `a127 project undeploy`', function(done) {
+    exec('127 project undeploy', {cwd: cwd}, function(error, stdout, stderr) {
+      expect(error).to.be.falsy;
+      expect(stderr).to.be.falsy;
+      done();
+    });
+  });
+
+  deleteAccount();
+
+  it('kills the server process', function(done) {
+    running(serverProccess.pid, function(err, live) {
+      if (live) {
+        process.kill(-serverProccess.pid, 'SIGTERM');
+      }
+      expect(serverProccess.connected).to.be.false;
+      serverProccess = null;
+      done();
+    });
+  });
+});
