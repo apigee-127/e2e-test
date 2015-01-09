71a8df906c7c534cd5dcf0de52368f4836d01dfc
Mohsen Azimi <msn.azi@gmail.com>
2015-01-09 11:43:05 -0800

    Make a request to deployed API to make sure it's working

--- test/4.deployment.js
+++ test/4.deployment.js
@@ -2,6 +2,7 @@
 
 var exec = require('child_process').exec;
 var chai = require('chai');
+var request = require('request');
 var expect = chai.expect;
 var deleteAccount = require('./3.account').delete;
 var cwd = require('./2.project').getCWD();
@@ -11,6 +12,15 @@ describe('deployment', function() {
     exec('127 project deploy', {cwd: cwd}, function(error, stdout, stderr) {
       expect(error).to.be.falsy;
       expect(stderr).to.be.falsy;
+      done();
+    });
+  });
+
+  it('makes a call to deployed API to make sure deployed API is working', function(done) {
+    var url = 'http://mazimi_a127-test.apigee.net/hello-world/my-path?name=Bart&last=Simpson';
+    request(url, function(error, resp, body) {
+      expect(error).to.be.falsy;
+      expect(body).to.contain('Hello, Bart Simpson');
       done();
     });
   });
